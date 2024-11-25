00939b2595a7a192b948da2bd433d33fa80ad724
hiran0dol <160134385+hiran0dol@users.noreply.github.com>
2024-11-25 12:00:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'Uma obra enorme amarela'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Curitiba'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'É oq ta na sua carteira de trabalho'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
